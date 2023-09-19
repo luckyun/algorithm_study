7e53c2bd20738726530b636f851077d4422db26d
luckyun <devjylee@gmail.com>
2023-09-20 00:12:42 +0900

    1699 제곱수의 합

--- src/main/java/baekjun/dp/SqrtSum1699.java
+++ src/main/java/baekjun/dp/SqrtSum1699.java
@@ -13,24 +13,17 @@ public class SqrtSum1699 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
-        int finNum = (int) Math.sqrt(n);
-
         int[] dp = new int[n+1];
+        int fNum = (int) Math.sqrt(n);
 
-
-        for (int i = 1; i <= n; i++) {
+        for (int i = 1; i <= n ; i++) {
             dp[i] = i;
         }
-
-        for (int i = 1; i <= finNum; i++) {
-            for (int j = i*i ; j <= n; j++) {
-                if(j-i*i >=0){
-                    dp[j] = Math.min(dp[j], dp[j-i*i]+1);
-                }
-
+        for (int i = 2; i <= fNum ; i++) {
+            for (int j = i*i; j <= n; j++) {
+                dp[j] = Math.min(dp[j], dp[j-i*i]+1);
             }
         }
-
-       System.out.println(dp[n]);
+        System.out.println(dp[n]);
     }
 }
